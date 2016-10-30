5dbba9fd843b76ac4ec5eb16d328146a1253cbfa
Nathan Guenther <nathanpguenther@gmail.com>
2016-10-30 22:19:13 +0000

    Submit solution for 'leap'

--- go/leap/leap.go
+++ go/leap/leap.go
@@ -1,12 +1,22 @@
 // Leap stub file
 
-// The package name is expected by the test program.
+// Package leap determines if a given year is a leap year.
 package leap
 
 // testVersion should match the targetTestVersion in the test file.
 const testVersion = 2
 
-// It's good style to write a comment here documenting IsLeapYear.
-func IsLeapYear(int) bool {
+// IsLeapYear takes in a year and returns T/F if a leap year
+func IsLeapYear(year int) bool {
 	// Write some code here to pass the test suite.
+	if year%4 == 0 {
+		if year%100 == 0 {
+			if year%400 == 0 {
+				return true
+			}
+			return false
+		}
+		return true
+	}
+	return false
 }
